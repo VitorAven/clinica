ba21005c9d4b87e10c9c53393cae9941024a07ae
VitorAven <vitorhugobassetto@gmail.com>
2016-06-07 20:30:18 -0300

    mto loko2

--- teste.php
+++ teste.php
@@ -3,7 +3,7 @@
 <html>
 
     <head>
-        <!-- Configuração de Charset -->
+        <!-- testeConfiguração de Charset -->
         <meta charset='utf-8' />
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <!-- Título da Página -->
