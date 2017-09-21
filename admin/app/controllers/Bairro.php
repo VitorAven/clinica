b16715ad78feba6d4b900019e009a56e84500a40
veracamargo <vera.camargo@fatecourinhos.edu.br>
2017-09-21 20:25:02 -0300

    Update Bairro.php

--- admin/app/controllers/Bairro.php
+++ admin/app/controllers/Bairro.php
@@ -4,7 +4,7 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Bairro extends CI_Controller {
-
+//teste
     /**
      * @author Vitor Hugo Bassetto <vitorhugobassetto@gmail.com>
      * 
